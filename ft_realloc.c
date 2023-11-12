74d26d05194b4b8ed77efdfd2cbd23ecd1ed6dbc
Krijn Schelvis <krijn.schelvis@gmail.com>
2023-11-12 20:54:32 +0100

    changed realloc

--- ft_realloc.c
+++ ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: krijn <krijn@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/11 12:28:41 by krijn         #+#    #+#                 */
-/*   Updated: 2023/11/11 12:47:29 by krijn         ########   odam.nl         */
+/*   Updated: 2023/11/12 20:54:13 by krijn         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,12 @@
 void	*ft_realloc(void *ptr, size_t size)
 {
 	void	*new_ptr;
-	size_t	old_size;
-	char	*temp;
 
 	if (ptr == NULL)
 		return (malloc(size));
+	if (!size)
+		return (ptr);
 	new_ptr = malloc(size);
-	if (new_ptr == NULL)
-		return (NULL);
-	old_size = 0;
-	temp = (char *)ptr;
-	while (*temp++)
-		old_size++;
-	if (size > old_size)
-		size = old_size;
-	ft_memmove(new_ptr, ptr, size);
-	free(ptr);
+	ft_memcpy(new_ptr, ptr, size);
 	return (new_ptr);
 }
