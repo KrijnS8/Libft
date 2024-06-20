5854e7be7c4a34d32c30bc0315a6e4c7e7c11237
Krijn Schelvis <kschelvi@f1r2s5.codam.nl>
2024-06-20 14:21:16 +0200

    added null check

--- ft_build_path.c
+++ ft_build_path.c
@@ -6,7 +6,7 @@
 /*   By: kschelvi <kschelvi@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/01/08 17:28:27 by kschelvi      #+#    #+#                 */
-/*   Updated: 2024/01/08 17:33:25 by kschelvi      ########   odam.nl         */
+/*   Updated: 2024/06/20 14:20:59 by kschelvi      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_build_path(char *folder, char *file, char *extention)
 	char	*path;
 	char	*temp;
 
+	if (!folder || !file || !extention)
+		return (NULL);
 	temp = ft_strjoin(folder, file);
 	if (!temp)
 		return (NULL);
